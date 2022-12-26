96857eb860cfaa4af4706f33aac660c9af74fc4b
Ibraheem Abdul-Malik <ibraheem4@gmail.com>
2022-12-26 18:59:28 -0500

    Revert "feat: bind to all network interfaces"
    This reverts commit 72d7c5d20730e149e50f0ea4ad9e89dcf2478eed.

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,4 +1,5 @@
 import { AuthProvider } from '@redwoodjs/auth'
+
 import { FatalErrorBoundary, RedwoodProvider } from '@redwoodjs/web'
 import { RedwoodApolloProvider } from '@redwoodjs/web/apollo'
 
@@ -11,13 +12,10 @@ import './index.css'
 const App = () => (
   <FatalErrorBoundary page={FatalErrorPage}>
     <RedwoodProvider titleTemplate="%PageTitle | %AppTitle">
-      <AuthProvider
-        type="dbAuth"
-        config={{ fetchConfig: { credentials: 'include' } }}
-      >
-        <RedwoodApolloProvider
-          graphQLClientConfig={{ httpLinkConfig: { credentials: 'include' } }}
-        >
+      <AuthProvider type="dbAuth" config={{ fetchConfig: { credentials: 'include' } }}>
+
+        <RedwoodApolloProvider graphQLClientConfig={{ httpLinkConfig: { credentials: 'include' }}} >
+
           <Routes />
         </RedwoodApolloProvider>
       </AuthProvider>
