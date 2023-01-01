e50aad6b6221f05d8da7234d25335b20ebafa73e
Ibraheem Abdul-Malik <ibraheem4@gmail.com>
2023-01-01 11:31:57 -0500

    test: add tests for ArticleCell, Article
    - make cell Success mocks more explicit
    - add test for summary={false}

--- web/src/components/ArticleCell/ArticleCell.stories.tsx
+++ web/src/components/ArticleCell/ArticleCell.stories.tsx
@@ -15,8 +15,8 @@ export const failure: ComponentStory<typeof Failure> = (args) => {
   return Failure ? <Failure error={new Error('Oh no')} {...args} /> : <></>
 }
 
-export const success: ComponentStory<typeof Success> = (args) => {
-  return Success ? <Success {...standard()} {...args} /> : <></>
+export const success: ComponentStory<typeof Success> = () => {
+  return Success ? <Success article={standard().article} /> : null
 }
 
 export default { title: 'Cells/ArticleCell' }
