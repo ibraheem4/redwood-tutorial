76ade8121b7911fb84cfa609f6ddffcaf5ba6491
Ibraheem Abdul-Malik <ibraheem4@gmail.com>
2023-01-06 06:19:10 -0500

    feat: add types to post scenario

--- api/src/services/posts/posts.scenarios.ts
+++ api/src/services/posts/posts.scenarios.ts
@@ -1,4 +1,8 @@
-export const standard = defineScenario({
+import type { Prisma, Post } from '@prisma/client'
+
+import type { ScenarioData } from '@redwoodjs/testing/api'
+
+export const standard = defineScenario<Prisma.PostCreateArgs>({
   post: {
     one: {
       data: {
@@ -34,3 +38,5 @@ export const standard = defineScenario({
     },
   },
 })
+
+export type StandardScenario = ScenarioData<Post, 'post'>
