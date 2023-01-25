66cb6c2ef5860d8d00e856aed7b7db672ea33f07
Ibraheem Abdul-Malik <ibraheem4@gmail.com>
2023-01-25 18:18:53 -0500

    build: update seeds file to reduce hardcoding
    - install chance

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -1,136 +1,89 @@
 import type { Prisma } from '@prisma/client'
 import { db } from 'api/src/lib/db'
+import Chance from 'chance'
 import CryptoJS from 'crypto-js'
 
 const ADMIN_PASSWORD = 'AdminPassword'
 const MODERATOR_PASSWORD = 'ModeratorPassword'
 
+const chance = new Chance()
+
 export default async () => {
   try {
-    const [hashedPasswordAdmin, saltAdmin] = _hashPassword(ADMIN_PASSWORD)
-    const [hashedPasswordModerator, saltModerator] =
-      _hashPassword(MODERATOR_PASSWORD)
-    const userData: Prisma.UserCreateArgs['data'][] = [
+    const users = [
       {
-        name: 'Admin User',
+        name: 'admin',
         email: 'admin@admin.com',
-        hashedPassword: hashedPasswordAdmin,
-        salt: saltAdmin,
-        resetToken: null,
-        resetTokenExpiresAt: null,
+        password: ADMIN_PASSWORD,
         roles: ['admin'],
-        posts: {
-          create: [
-            {
-              title: 'Welcome to the blog!',
-              body: "I'm baby single- origin coffee kickstarter lo - fi paleo skateboard.Tumblr hashtag austin whatever DIY plaid knausgaard fanny pack messenger bag blog next level woke.Ethical bitters fixie freegan,helvetica pitchfork 90's tbh chillwave mustache godard subway tile ramps art party. Hammock sustainable twee yr bushwick disrupt unicorn, before they sold out direct trade chicharrones etsy polaroid hoodie. Gentrify offal hoodie fingerstache.",
-              comments: {
-                create: [
-                  {
-                    name: 'Mario Mario',
-                    body: 'Mamma mia!',
-                  },
-                  {
-                    name: 'Luigi Mario',
-                    body: 'Okey-dokey!',
-                  },
-                  {
-                    name: 'Yoshi',
-                    body: 'Alright!',
-                  },
-                ],
-              },
-            },
-            {
-              title: 'A little more about me',
-              body: "Raclette shoreditch before they sold out lyft. Ethical bicycle rights meh prism twee. Tote bag ennui vice, slow-carb taiyaki crucifix whatever you probably haven't heard of them jianbing raw denim DIY hot chicken. Chillwave blog succulents freegan synth af ramps poutine wayfarers yr seitan roof party squid. Jianbing flexitarian gentrify hexagon portland single-origin coffee raclette gluten-free. Coloring book cloud bread street art kitsch lumbersexual af distillery ethical ugh thundercats roof party poke chillwave. 90's palo santo green juice subway tile, prism viral butcher selvage etsy pitchfork sriracha tumeric bushwick.",
-            },
-          ],
-        },
       },
       {
-        name: 'Moderator User',
+        name: 'moderator',
         email: 'moderator@moderator.com',
-        hashedPassword: hashedPasswordModerator,
-        salt: saltModerator,
-        resetToken: null,
-        resetTokenExpiresAt: null,
+        password: MODERATOR_PASSWORD,
         roles: ['moderator'],
-        posts: {
-          create: [
-            {
-              title: 'Welcome to the blog!',
-              body: "I'm baby single- origin coffee kickstarter lo - fi paleo skateboard.Tumblr hashtag austin whatever DIY plaid knausgaard fanny pack messenger bag blog next level woke.Ethical bitters fixie freegan,helvetica pitchfork 90's tbh chillwave mustache godard subway tile ramps art party. Hammock sustainable twee yr bushwick disrupt unicorn, before they sold out direct trade chicharrones etsy polaroid hoodie. Gentrify offal hoodie fingerstache.",
-              comments: {
-                create: [
-                  {
-                    name: 'Mario Mario',
-                    body: 'Mamma mia!',
-                  },
-                  {
-                    name: 'Luigi Mario',
-                    body: 'Okey-dokey!',
-                  },
-                  {
-                    name: 'Yoshi',
-                    body: 'Alright!',
-                  },
-                ],
-              },
-            },
-            {
-              title: 'A little more about me',
-              body: "Raclette shoreditch before they sold out lyft. Ethical bicycle rights meh prism twee. Tote bag ennui vice, slow-carb taiyaki crucifix whatever you probably haven't heard of them jianbing raw denim DIY hot chicken. Chillwave blog succulents freegan synth af ramps poutine wayfarers yr seitan roof party squid. Jianbing flexitarian gentrify hexagon portland single-origin coffee raclette gluten-free. Coloring book cloud bread street art kitsch lumbersexual af distillery ethical ugh thundercats roof party poke chillwave. 90's palo santo green juice subway tile, prism viral butcher selvage etsy pitchfork sriracha tumeric bushwick.",
-            },
-          ],
-        },
       },
     ]
 
-    // Note: if using PostgreSQL, using `createMany` to insert multiple records is much faster
-    // @see: https://www.prisma.io/docs/reference/api-reference/prisma-client-reference#createmany
-    Promise.all(
-      userData.map(async (data: Prisma.UserCreateArgs['data']) => {
-        const record = await db.user.create({ data })
-        console.log(record)
+    for (const user of users) {
+      const [hashedPassword, salt] = _hashPassword(user.password)
+      await db.user.create({
+        data: {
+          name: user.name,
+          email: user.email,
+          hashedPassword,
+          salt,
+          roles: user.roles,
+          posts: {
+            create: [
+              {
+                title: chance.sentence(),
+                body: chance.paragraph(),
+                comments: {
+                  create: [
+                    {
+                      name: chance.name(),
+                      body: chance.sentence(),
+                    },
+                    {
+                      name: chance.name(),
+                      body: chance.sentence(),
+                    },
+                    {
+                      name: chance.name(),
+                      body: chance.sentence(),
+                    },
+                  ],
+                },
+              },
+              {
+                title: chance.sentence(),
+                body: chance.paragraph(),
+              },
+            ],
+          },
+        },
       })
-    )
-
-    console.info('')
-    console.info('  Seeded admin user:')
-    console.info('')
-    console.info('    Name: Admin User')
-    console.info('    Email: admin@admin.com')
-    console.info(`    Password: ${ADMIN_PASSWORD}`)
-    console.info('')
-    console.info(`  (Please don't use this login in a production environment)`)
-    console.info('')
-
-    console.info('')
-    console.info('  Seeded moderator user:')
-    console.info('')
-    console.info('    Name: Moderator User')
-    console.info(`    Password: ${MODERATOR_PASSWORD}`)
-    console.info('    Password: ModeratorPassword')
-    console.info('')
-    console.info(`  (Please don't use this login in a production environment)`)
-    console.info('')
-
+      console.info(
+        `- Created user: ${user.name} with password: ${user.password}`
+      )
+      console.info(`- Please don't use this login in a production environment`)
+    }
     const contactData: Prisma.ContactCreateArgs['data'][] = [
       {
-        name: 'Mario Mario',
-        email: 'mario@mariobros.com',
-        message: 'Mamma mia!',
+        name: chance.name(),
+        email: chance.email(),
+        message: chance.sentence(),
       },
       {
-        name: 'Luigi Mario',
-        email: 'luigi@mariobros.com',
-        message: 'Okey-dokey!',
+        name: chance.name(),
+        email: chance.email(),
+        message: chance.sentence(),
       },
       {
-        name: 'Yoshi',
-        email: 'yoshi@mariobros.com',
-        message: 'Alright!',
+        name: chance.name(),
+        email: chance.email(),
+        message: chance.sentence(),
       },
     ]
 
