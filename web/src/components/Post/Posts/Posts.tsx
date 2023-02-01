79f4c102b234f498c87dd663320b5a66dd67a943
Ibraheem Abdul-Malik <ibraheem4@gmail.com>
2023-01-31 21:58:59 -0500

    Revert "feat: generate post component"
    This reverts commit 93335f509efeba77105776cb555face1d7ceff2f.

--- web/src/components/Post/Posts/Posts.tsx
+++ web/src/components/Post/Posts/Posts.tsx
@@ -1,8 +1,41 @@
-import type { FindPosts } from 'types/graphql'
+import type { DeletePostMutationVariables, FindPosts } from 'types/graphql'
 
-import Post from '../Post/Post'
+import { Link, routes } from '@redwoodjs/router'
+import { useMutation } from '@redwoodjs/web'
+import { toast } from '@redwoodjs/web/toast'
+
+import { QUERY } from 'src/components/Post/PostsCell'
+import { timeTag, truncate } from 'src/lib/formatters'
+
+const DELETE_POST_MUTATION = gql`
+  mutation DeletePostMutation($id: String!) {
+    deletePost(id: $id) {
+      id
+    }
+  }
+`
 
 const PostsList = ({ posts }: FindPosts) => {
+  const [deletePost] = useMutation(DELETE_POST_MUTATION, {
+    onCompleted: () => {
+      toast.success('Post deleted')
+    },
+    onError: (error) => {
+      toast.error(error.message)
+    },
+    // This refetches the query on the list page. Read more about other ways to
+    // update the cache over here:
+    // https://www.apollographql.com/docs/react/data/mutations/#making-all-other-cache-updates
+    refetchQueries: [{ query: QUERY }],
+    awaitRefetchQueries: true,
+  })
+
+  const onDeleteClick = (id: DeletePostMutationVariables['id']) => {
+    if (confirm('Are you sure you want to delete post ' + id + '?')) {
+      deletePost({ variables: { id } })
+    }
+  }
+
   return (
     <div className="rw-segment rw-table-wrapper-responsive">
       <table className="rw-table">
@@ -17,7 +50,38 @@ const PostsList = ({ posts }: FindPosts) => {
         </thead>
         <tbody>
           {posts.map((post) => (
-            <Post key={post.id} post={post} />
+            <tr key={post.id}>
+              <td>{truncate(post.id)}</td>
+              <td>{truncate(post.title)}</td>
+              <td>{truncate(post.body)}</td>
+              <td>{timeTag(post.createdAt)}</td>
+              <td>
+                <nav className="rw-table-actions">
+                  <Link
+                    to={routes.post({ id: post.id })}
+                    title={'Show post ' + post.id + ' detail'}
+                    className="rw-button rw-button-small"
+                  >
+                    Show
+                  </Link>
+                  <Link
+                    to={routes.editPost({ id: post.id })}
+                    title={'Edit post ' + post.id}
+                    className="rw-button rw-button-small rw-button-blue"
+                  >
+                    Edit
+                  </Link>
+                  <button
+                    type="button"
+                    title={'Delete post ' + post.id}
+                    className="rw-button rw-button-small rw-button-red"
+                    onClick={() => onDeleteClick(post.id)}
+                  >
+                    Delete
+                  </button>
+                </nav>
+              </td>
+            </tr>
           ))}
         </tbody>
       </table>
